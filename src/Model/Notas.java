a0545292cabbde544519e1408e3dc2523db9fbaf
Anomium <melvin.martinez.97@gmail.com>
2018-02-19 18:39:19 -0500

    Creacion del Modelo

--- src/Model/Notas.java
+++ src/Model/Notas.java
@@ -1,4 +1,31 @@
 package Model;
 public class Notas {
-
+    
+    private String materia;
+    private double nota;
+    
+    public Notas (){
+        
+    }
+    public Notas(String materia, double nota){
+        this.materia = materia;
+        this.nota = nota;
+    }
+    
+    //Set
+    public void setMateria(String materia){
+        this.materia = materia;
+    }
+    public void setNota(double nota){
+        this.nota = nota;
+    }
+    
+    //get
+    public String getMateria(){
+        return materia;
+    }
+    public double getNota(){
+        return nota;
+    }
+    
 }
